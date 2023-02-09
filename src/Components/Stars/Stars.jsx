45a868c3c9153d75a77f03de53e10a1cbf8dc1a3
Marc S <marcsalaver@gmail.com>
2023-02-09 14:04:12 -0800

    first commit

---
+++ src/Components/Stars/Stars.jsx
@@ -0,0 +1,356 @@
+import React from "react";
+import { Col } from "react-bootstrap";
+
+export default function Stars(props) {
+  switch (props.priority) {
+    case "0":
+      return (
+        <Col>
+          <i
+            id="1"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+          <i
+            id="2"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+          <i
+            id="3"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+          <i
+            id="4"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+          <i
+            id="5"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+        </Col>
+      );
+    case "1":
+      return (
+        <Col>
+          <i
+            id="1"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+            }}
+            className="fa-solid fa-star"
+          ></i>
+          <i
+            id="2"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+          <i
+            id="3"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+          <i
+            id="4"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+          <i
+            id="5"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+        </Col>
+      );
+    case "2":
+      return (
+        <Col>
+          <i
+            id="1"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-solid fa-star"
+          ></i>
+          <i
+            id="2"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-solid fa-star"
+          ></i>
+          <i
+            id="3"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+          <i
+            id="4"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+          <i
+            id="5"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+        </Col>
+      );
+    case "3":
+      return (
+        <Col>
+          <i
+            id="1"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-solid fa-star"
+          ></i>
+          <i
+            id="2"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-solid fa-star"
+          ></i>
+          <i
+            id="3"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-solid fa-star"
+          ></i>
+          <i
+            id="4"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+          <i
+            id="5"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+        </Col>
+      );
+    case "4":
+      return (
+        <Col>
+          <i
+            id="1"
+            onClick={(e) => {
+              props.priority = e.target.id;
+
+              props.setNumber(e.target.id)
+            }}
+            className="fa-solid fa-star"
+          ></i>
+          <i
+            id="2"
+            onClick={(e) => {
+              props.priority = e.target.id;
+
+              props.setNumber(e.target.id)
+            }}
+            className="fa-solid fa-star"
+          ></i>
+          <i
+            id="3"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-solid fa-star"
+          ></i>
+          <i
+            id="4"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-solid fa-star"
+          ></i>
+          <i
+            id="5"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+        </Col>
+      );
+    case "5":
+      return (
+        <Col>
+          <i
+            id="1"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-solid fa-star"
+          ></i>
+          <i
+            id="2"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-solid fa-star"
+          ></i>
+          <i
+            id="3"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-solid fa-star"
+          ></i>
+          <i
+            id="4"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-solid fa-star"
+          ></i>
+          <i
+            id="5"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-solid fa-star"
+          ></i>
+        </Col>
+      );
+    default:
+      return (
+        <Col>
+          <i
+            id="1"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+          <i
+            id="2"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+          <i
+            id="3"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+          <i
+            id="4"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+          <i
+            id="5"
+            onClick={(e) => {
+              props.priority = e.target.id;
+              props.setNumber(e.target.id)
+
+            }}
+            className="fa-regular fa-star"
+          ></i>
+        </Col>
+      );
+  }
+}

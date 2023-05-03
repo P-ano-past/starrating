45456c593595024abf2e2a4317e73b525be6b59c
Marc S <39275810+P-ano-past@users.noreply.github.com>
2023-05-03 21:24:09 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,67 @@
-import React , {useState}from 'react'
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import './App.css'
-import Stars from './Components/Stars/Stars';
+import React, { useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import "./App.css";
+import Stars from "./Components/Stars/Stars";
 
 function App() {
-  const [priority, setNumber] = useState("1")
-
+  const [priority, setNumber] = useState("1");
 
   return (
     <div className="App">
-      <h1>hello</h1>
-      {/* <p>This is to demonstrate the use case of state handling and passing of props while also illustrating a simple 5 star rating system.</p> */}
-      <p>This is to demonstrate the use case of state handling and passing of props while also illustrating a simple 5 star rating system.</p>
+      <h1>Hello</h1>
+      <p>
+        This a demonstration of state handling and passing of
+        props while also illustrating a simple 5 star rating system.
+      </p>
       <Container>
         <Row>
-        <Col><Button onClick={(e => {setNumber(e.target.value)})} value="1">1</Button>
-        <Button onClick={(e => {setNumber(e.target.value)})} value="2">2</Button>
-        <Button onClick={(e => {setNumber(e.target.value)})} value="3">3</Button>
-        <Button onClick={(e => {setNumber(e.target.value)})} value="4">4</Button>
-        <Button onClick={(e => {setNumber(e.target.value)})} value="5">5</Button></Col>
-
-
+          <Col>
+            <Button
+              onClick={(e) => {
+                setNumber(e.target.value);
+              }}
+              value="1"
+            >
+              1
+            </Button>
+            <Button
+              onClick={(e) => {
+                setNumber(e.target.value);
+              }}
+              value="2"
+            >
+              2
+            </Button>
+            <Button
+              onClick={(e) => {
+                setNumber(e.target.value);
+              }}
+              value="3"
+            >
+              3
+            </Button>
+            <Button
+              onClick={(e) => {
+                setNumber(e.target.value);
+              }}
+              value="4"
+            >
+              4
+            </Button>
+            <Button
+              onClick={(e) => {
+                setNumber(e.target.value);
+              }}
+              value="5"
+            >
+              5
+            </Button>
+          </Col>
         </Row>
-        </Container>
-      <Stars
-        priority={priority}
-        setNumber={setNumber}
-      />
+      </Container>
+      <Stars priority={priority} setNumber={setNumber} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
